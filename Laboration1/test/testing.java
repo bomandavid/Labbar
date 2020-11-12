556d22543c576a8578b99e3da9894e5ebaf88570
L0ngben <henke5000@gmail.com>
2020-11-12 17:17:53 +0100

    tillagda tester

--- Laboration1/test/testing.java
+++ Laboration1/test/testing.java
@@ -1,8 +1,14 @@
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class testing {
+    @Before
+    public void beforeT(){
+        Volvo240 volvo = new Volvo240();
+        Saab95 saab = new Saab95();
+    }
 
     @Test
     public void testVolvoGas() {
@@ -18,6 +24,82 @@ public class testing {
     }
 
     @Test
+    public void testSaabSpeedFactor(){
+        Saab95 saab = new Saab95();
+        saab.setEnginePower(125);
+        saab.setTurboOn();
+        assertEquals(125*0.01*1.3,saab.speedFactor());
+    }
+
+    @Test
+    public void testSaabIncrementSpeed(){
+        Saab95 saab = new Saab95();
+        saab.startEngine();
+        double amount = 25;
+        saab.incrementSpeed(amount);
+        assertEquals(125*0.01*amount+0.1,saab.getCurrentSpeed());
+    }
+
+    @Test
+    public void testSaabDecrementSpeed(){
+        Saab95 saab = new Saab95();
+        saab.setCurrentSpeed(10);
+        double amount = 5;
+        saab.decrementSpeed(amount);
+        assertEquals(10-0.01*125*amount,saab.getCurrentSpeed());
+    }
+
+    @Test
+    public void testVolvoSpeedFactor(){
+        Volvo240 volvo = new Volvo240();
+        volvo.setEnginePower(200);
+        assertEquals(200*0.01*1.25,volvo.speedFactor());
+    }
+
+    @Test
+    public void testVolvoIncrementSpeed(){
+        Volvo240 volvo = new Volvo240();
+        volvo.startEngine();
+        double amount = 25;
+        volvo.incrementSpeed(amount);
+        assertEquals(100*0.01*1.25*amount+0.1,volvo.getCurrentSpeed());
+    }
+
+    @Test
+    public void testVolvoDecrementSpeed(){
+        Volvo240 volvo = new Volvo240();
+        volvo.setCurrentSpeed(10);
+        double amount = 5;
+        volvo.decrementSpeed(amount);
+        assertEquals(10-1.25*amount,volvo.getCurrentSpeed());
+    }
+
+    @Test
+    public void testSetCurrentSpeed(){
+
+    }
+
+    @Test
+    public void testBrake(){
+
+    }
+
+    @Test
+    public void testMove(){}
+
+    @Test
+    public void testTurnRight(){
+        Car volvo = new Volvo240();
+        volvo.turnRight();
+        assertEquals(1, volvo.getDir());
+    }
+
+    @Test
+    public void testTurnLeft(){
+    }
+
+    /*
+    @Test
     public void test(){
         Car saab = new Saab95();
         saab.startEngine();
@@ -26,21 +108,13 @@ public class testing {
         saab.getDir();
         saab.turnRight();
     }
+    */
 
     @Test
     public void testNrDoors(){
         Car volvo = new Volvo240();
         assertEquals(4, volvo.getNrDoors());
     }
-    @Test
-    public void testDir(){
-        Car volvo = new Volvo240();
-        volvo.turnRight();
-        assertEquals(1, volvo.getDir());
-    }
 
-    @Test
-    public void testGas() {
 
-    }
 }
